e47360b181f5ad4a56cc6264cbb9dbc7e3354dfe
ViniciussAlencar <vinicius.alencaarr@gmail.com>
2020-12-25 02:10:04 -0300

    Inserindo componentes iniciais do app

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  View
+} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.entradas}>
+        <TextInput placeholder="Altura" keyboardType="numeric" style={styles.input}></TextInput>
+        <TextInput placeholder="Massa" keyboardType="numeric" style={styles.input}></TextInput>
+      </View>  
+      <TouchableOpacity style={styles.button} onPress={()=>{}} style={styles.buttonText}><Text>Calcular</Text></TouchableOpacity>
     </View>
   );
 }
@@ -15,7 +24,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  entradas: {
+    flexDirection: "row",
+  },
+  input: {
+    height: 80,
+    textAlign: "center",
+    width: "50%",
+    fontSize: 50,
+    marginTop: 24,
+  },
+  buttonText: {
+    alignSelf: "center",
+  },
+  button: {
+    backgroundColor: "tomato",
+    padding: 20,
+
   },
 });
