6528416a215f360cc3a1503afb4af92f3be35d32
ViniciussAlencar <vinicius.alencaarr@gmail.com>
2020-12-25 13:04:46 -0300

    finalizando interface

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import {
   View
 } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -15,15 +16,16 @@ export default function App() {
         <TextInput placeholder="Altura" keyboardType="numeric" style={styles.input}></TextInput>
         <TextInput placeholder="Massa" keyboardType="numeric" style={styles.input}></TextInput>
       </View>  
-      <TouchableOpacity style={styles.button} onPress={()=>{}} style={styles.buttonText}><Text>Calcular</Text></TouchableOpacity>
+      <TouchableOpacity style={styles.buttonText} onPress={()=>{}}><Text style={styles.content}>Calcular</Text></TouchableOpacity>
+      <Text style={styles.resultado}>30</Text>
+      <Text style={[styles.resultado, {fontSize:24}]}>Obesidade Morbída</Text>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop: 100,
   },
   entradas: {
     flexDirection: "row",
@@ -34,13 +36,25 @@ const styles = StyleSheet.create({
     width: "50%",
     fontSize: 50,
     marginTop: 24,
+    marginBottom: 25,
+  },
+  resultado: {
+    color: "#000",
+    alignSelf: "center",
+    color: "#232323",
+    fontSize: 65,
+    padding: 15,
+
   },
   buttonText: {
     alignSelf: "center",
+    padding: 30,
+    backgroundColor: "rgb(100,240,245)",
+    fontSize: 20,
+    borderRadius: 20,
   },
-  button: {
-    backgroundColor: "tomato",
-    padding: 20,
-
-  },
+  content: {
+    color: "#fff",
+    fontWeight: "bold",
+  }
 });
